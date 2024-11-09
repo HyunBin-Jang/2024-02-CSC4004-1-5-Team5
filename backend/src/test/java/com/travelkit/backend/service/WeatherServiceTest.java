deff5f0c050e722d37e8f26eabb8d403bb902b37
janghb <janghb3730@gmail.com>
2024-11-09 18:45:38 +0900

    BE:[test] 도시이름을 경도, 위도로 변환 후 API Request

--- backend/src/test/java/com/travelkit/backend/service/WeatherServiceTest.java
+++ backend/src/test/java/com/travelkit/backend/service/WeatherServiceTest.java
@@ -1,7 +1,7 @@
 package com.travelkit.backend.service;
-
 import org.junit.jupiter.api.Test;
 
+import java.io.IOException;
 import java.util.Map;
 
 public class WeatherServiceTest {
@@ -10,6 +10,12 @@ public class WeatherServiceTest {
     public void requestWeather(){
         Map<String, Object> result =            // Seoul test
                 weatherService.getForecastByCoordinates(37.5665, 126.9780);
+        System.out.println(result);
+    }
+    @Test
+    public void requestWeatherByCity() throws IOException {
+        Map<String, Object> result =
+                weatherService.getForecastByCity("Tokyo");
         System.out.println(result);
     }
 }
