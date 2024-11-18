29c3d83ab2fb727e554eb4133f1e5e8a8173056c
Jang HyunBin <janghb3730@gmail.com>
2024-11-18 22:13:11 +0900

    BE:[fix] Entity 수정(출발날짜, 도착날짜)

--- backend/src/main/java/com/travelkit/backend/domain/Checklist.java
+++ backend/src/main/java/com/travelkit/backend/domain/Checklist.java
@@ -6,6 +6,7 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Entity
@@ -24,6 +25,10 @@ public class Checklist {
 
     @Embedded
     private Destination destination;
+
+    private LocalDate departureDate;
+
+    private LocalDate arrivalDate;
 
     @JsonManagedReference
     @OneToMany(cascade = CascadeType.ALL, mappedBy = "checklist")
